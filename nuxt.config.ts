71089a6e7a4d0ef396d1e3450b068de5d7d1afff
Christopher Gonçalves <chrissgon.dev@gmail.com>
2024-04-06 20:41:09 +0100

    :sparkles: feat: remove zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,13 @@ export default defineNuxtConfig({
         },
       ],
 
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        }
+      ],
+
       script: [
         {
           src: "/lazy-icons.js",
